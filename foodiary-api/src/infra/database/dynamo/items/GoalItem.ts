31f6e92175250c96146b12b0dd451559c657ed45
eduardovsousa <eduardo.varela@outlook.com.br>
2026-09-15 16:09:29 -0300

    feat: create Goal Entity and GoalItem

---
+++ foodiary-api/src/infra/database/dynamo/items/GoalItem.ts
@@ -0,0 +1,68 @@
+import { Goal } from '@application/entities/Goal.js';
+import { AccountItem } from './AccountItem.js';
+
+export class GoalItem {
+  private readonly type = 'Goal';
+
+  private readonly keys: GoalItem.Keys;
+
+  constructor(private readonly attrs: GoalItem.Attributes) {
+    this.keys = {
+      PK: GoalItem.getPK(this.attrs.accountId),
+      SK: GoalItem.getSK(this.attrs.accountId),
+    };
+  }
+
+  toItem(): GoalItem.ItemType {
+    return {
+      ...this.attrs,
+      ...this.keys,
+      type: this.type,
+    };
+  }
+
+  static fromEntity(goal: Goal) {
+    return new GoalItem({
+      ...goal,
+      createdAt: goal.createdAt.toISOString(),
+    });
+  }
+
+  static toEntity(goalItem: GoalItem.ItemType) {
+    return new Goal({
+      accountId: goalItem.accountId,
+      calories: goalItem.calories,
+      carbohydrates: goalItem.carbohydrates,
+      fats: goalItem.fats,
+      proteins: goalItem.proteins,
+      createdAt: new Date(goalItem.createdAt),
+    });
+  }
+
+  static getPK(goalId: string): GoalItem.Keys['PK'] {
+    return `ACCOUNT#${goalId}`;
+  }
+  static getSK(goalId: string): GoalItem.Keys['SK'] {
+    return `ACCOUNT#${goalId}#GOAL`;
+  }
+}
+
+export namespace GoalItem {
+  export type Keys = {
+    PK: AccountItem.Keys['PK'];
+    SK: `ACCOUNT#${string}#GOAL`;
+  }
+
+  export type Attributes = {
+    accountId: string;
+    calories: number;
+    proteins: number;
+    carbohydrates: number;
+    fats: number;
+    createdAt: string;
+  }
+
+  export type ItemType = Keys & Attributes & {
+    type: 'Goal'
+  };
+}
