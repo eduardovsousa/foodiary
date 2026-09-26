03452e26038b08ad9d79206bda7527f1ad3d4180
eduardovsousa <eduardo.varela@outlook.com.br>
2026-09-26 12:38:04 -0300

    feat(theme): theme created for the project

---
+++ foodiary-app/src/ui/App.tsx
@@ -0,0 +1,43 @@
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  useFonts,
+} from '@expo-google-fonts/host-grotesk';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { theme } from './theme';
+
+export function App() {
+  const [isFontsLoaded] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+  });
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontFamily: theme.fontFamily.sans.semiBold,
+          fontSize: theme.fontSize.base,
+        }}
+      >Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.lime[500],
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
